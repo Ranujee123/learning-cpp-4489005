31189228850a5ec187a643ad19ad87a04ac39bc5
H.A.V.RANUJEE <117263085+Ranujee123@users.noreply.github.com>
2024-08-29 17:52:34 +0000

    new

--- src/Ch03/03_10b/CodeDemo.cpp
+++ src/Ch03/03_10b/CodeDemo.cpp
@@ -8,7 +8,16 @@
 #include "cow.h"
 
 int main(){
-    
+    std::vector<cow> cattle;
+
+    cattle.push_back(cow("Betty", 7,cow_purpose::meat));
+    cattle.push_back(cow("Honey", 2,cow_purpose::pet));
+    cattle.push_back(cow("Tuny", 17,cow_purpose::hide));
+    cattle.push_back(cow("Sony", 23,cow_purpose::dairy));
+
+    std::cout<<"The name of the first cow is "<< cattle.begin()->get_name()<<std::endl;
+    std::cout<<"At index 1 "<< cattle.begin()->get_name()<<std::endl;
+    std::cout<<"The name of the first cow is "<< cattle.begin()->get_name()<<std::endl;
     std::cout << std::endl << std::endl;
     return (0);
 }
